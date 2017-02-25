77e36ae1776e0de8af6c35784dcf5f5f2045e603
betamee <gongxq95@gmail.com>
2017-02-25 19:14:36 +0800

    更新下webpack.pro的配置

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -67,7 +67,7 @@ module.exports={
       },
 
       {//处理文字
-         test: /\.(woff|ttf|svg|eot')$/,
+         test: /\.(woff|ttf|svg|eot|woff2)$/,
         //  include:path.resolve(__dirname,'/client/assets'),
          use:[
            {
